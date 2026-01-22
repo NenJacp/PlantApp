54ab1aec96b262e2283f951f2561a469512d2c7a
NenDamaya <cruzjorge.059.yuc@gmail.com>
2026-01-21 23:27:01 -0600

    Initial commit

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Navbar from '$lib/components/NavBar.svelte';
+  import './../layout.css';
+  import favicon from '$lib/assets/favicon.svg';
+  import '../../app.css';
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
+
+<Navbar />
+{@render children()}
